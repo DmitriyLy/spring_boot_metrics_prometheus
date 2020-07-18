5c4abf73fe87be2f076b958867ccd378b038e517
DmitriyLy <dmitriy.lisay@gmail.com>
2020-07-18 12:38:23 +0300

    init

---
+++ src/test/java/dmly/micrometer/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package dmly.micrometer.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
